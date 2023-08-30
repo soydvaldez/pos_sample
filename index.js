6e015102b2fd53080876af7f63ebd9da0d1c616e
dvaldez <dvaldez@no-reply.com>
2023-08-30 14:51:03 -0600

    config: Se modico el archivo de rutas para exponer los components de la aplicacion

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-// Enrutador with vanilla javascript 
+// Enrutador con vanilla javascript 
 
 let ROUTES = [
-    { path: '/', value: 'product-table.html' },
-    { path: '/home', value: 'product-table.html' },
+    { path: '/', resource: 'src/components/products/product-table.html' },
+    { path: '/products', resource: 'src/components/products/product-table.html' },
 ];
 
+// , redirecTo: 'index.html' 
 // Pregunta por la ruta
+
 for (let route of ROUTES) {
     if (window.location.pathname === route.path) {
-        window.location.pathname = route.value;
+        window.location.pathname = route.resource;
     }
 }
 
+// redirect to product-resource: product resource change url value
 
-// window.location.href = '/index.html';
+// Esto  le pides al servidor: /products 
+// el servidor internamente sabe que el recurso esta en: src/components/products/product-table.html
--- js/data/config.js
+++
@@ -1,5 +0,0 @@
-let config_app = {
-    BASE_URI: 'http://localhost:3000'
-}
-
-export default config_app;
